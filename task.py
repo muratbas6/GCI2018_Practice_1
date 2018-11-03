8165aa3eb4dd4389d9811eb944f627593ec9ca18
muratbas6 <muratbas.dev@gmail.com>
2018-11-04 02:47:41 +0300

    first commit

---
+++ task.py
@@ -0,0 +1,8 @@
+for i in range(10):
+  print("GCI is great ")
+
+
+name = input("What is your name?")
+print("Hello "+name+",please to meet you!")
+reversed_name = name[::-1]
+print("Hello "+name+", please to meet you! Did you know that your name backwards is "+reversed_name)
